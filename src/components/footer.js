fb2b7060341190561392ade9377b288d03336b6f
miirkoD <dabarcicm@gmail.com>
2023-10-28 00:57:36 +0200

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './footer.css';
-import SocialIcons from './SocialIcons'; // Import the SocialIcons component
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesomeIcon
-import { faGithub } from '@fortawesome/free-brands-svg-icons'; // Import the GitHub icon
+import SocialIcons from './SocialIcon'; // Import the SocialIcons component
+
 
 const Footer = () => {
   return (
@@ -15,11 +14,7 @@ const Footer = () => {
           <p>Address: 123 Main St, City, Country</p>
         </div>
         <div className="footer-links">
-          <a href="https://www.yourportfoliowebsite.com">Portfolio</a>
           <SocialIcons /> 
-          <a href="https://github.com/yourusername">
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
         </div>
       </div>
       <div className="footer-copyright">
